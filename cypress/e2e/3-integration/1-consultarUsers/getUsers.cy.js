cb664ed982424c6521b5f56ef041089dec8cc274
gnfranco92 <guilhermenetofranco@ymail.com>
2025-01-28 19:50:44 -0300

    First

---
+++ cypress/e2e/3-integration/1-consultarUsers/getUsers.cy.js
@@ -0,0 +1,36 @@
+describe('Teste de API - Lista de Usuários', () => {
+  const apiUrl = 'https://serverest.dev/usuarios';
+
+  it('Deve retornar uma lista de usuários e validar a quantidade', () => {
+    cy.request({
+      method: 'GET',
+      url: apiUrl,
+    }).then((response) => {
+      // Verifica se o status code é 200
+      expect(response.status).to.eq(200);
+
+      // Verifica se o corpo da resposta contém a propriedade "usuarios"
+      expect(response.body).to.have.property('usuarios');
+
+      // Verifica se "usuarios" é um array
+      expect(response.body.usuarios).to.be.an('array');
+
+      // Verifica se a propriedade "quantidade" existe
+      expect(response.body).to.have.property('quantidade');
+
+      // Verifica se a quantidade é igual ao tamanho do array "usuarios"
+      expect(response.body.usuarios.length).to.eq(response.body.quantidade);
+
+      // Opcional: Verifique o primeiro usuário na lista (se existir)
+      if (response.body.usuarios.length > 0) {
+        const primeiroUsuario = response.body.usuarios[0];
+        expect(primeiroUsuario).to.have.property('_id');
+        expect(primeiroUsuario).to.have.property('nome');
+        expect(primeiroUsuario).to.have.property('email');
+        expect(primeiroUsuario).to.have.property('password');
+        expect(primeiroUsuario).to.have.property('administrador');
+      }
+    });
+  });
+});
+
---
+++ cypress/e2e/3-integration/3-consultarUser/getUserById.cy.js
@@ -0,0 +1,29 @@
+// cypress/e2e/consulta/consultarUsuario.cy.js
+describe('Teste de API - Consulta de Usuário', () => {
+    const apiUrl = 'https://serverest.dev/usuarios';
+  
+    it('Deve consultar o usuário cadastrado previamente', () => {
+      // Lê o ID do usuário salvo no arquivo JSON
+      cy.readFile('cypress/fixtures/userData.json').then((userData) => {
+        const userId = userData.id;
+  
+        // Realiza a consulta do usuário
+        cy.request({
+          method: 'GET',
+          url: `${apiUrl}/${userId}`,
+        }).then((response) => {
+          // Verifica se o status code é 200
+          expect(response.status).to.eq(200);
+  
+          // Valida os dados retornados
+          const user = response.body;
+          expect(user).to.have.property('_id', userId);
+          expect(user).to.have.property('nome', userData.nome);
+          expect(user).to.have.property('email', userData.email);
+  
+          cy.log('Consulta de usuário realizada com sucesso.');
+        });
+      });
+    });
+  });
+  

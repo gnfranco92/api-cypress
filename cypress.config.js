52dbd6694969995683f80cf2705346f21ceef19a
gnfranco92 <guilhermenetofranco@ymail.com>
2025-01-28 22:43:47 -0300

    Alterando o cypress.config.js para gerar o report

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,25 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
+    reporter: "mochawesome",
+    reporterOptions: {
+      reportDir: "cypress/reports",
+      overwrite: false,
+      html: false,
+      json: true
+    }
+  }
 });
+
+
+// const { defineConfig } = require("cypress");
+
+// module.exports = defineConfig({
+//   e2e: {
+    
+//     setupNodeEvents(on, config) {
+//       // implement node event listeners here
+      
+//     },
+//   },
+// });
